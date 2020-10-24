b505176fe3edf2fd258b243dd100a6c674679052
SergioEvando23 <sergioevandocosta@gmail.com>
2020-10-24 13:13:27 -0300

    adcionando e importando Api.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
- import React from 'react'
+ import React, { Component } from 'react'
+ import Api from './Api'
 
-function App() {
-  return (
-    <div>
-      <h1> OTIMO O APP TA PEGANDO  </h1> 
-    </div>
-  );
-}
 
+ class App extends Component{
+  state = {
+    pokemons: [],
+  }
+  async componentDidMount(){
+    const response = await Api.get('');
+    console.log(response.data);
+
+    this.setState({ pokemons: response.data});
+  }
+
+   render(){
+     return(
+       <div>
+         <h1>Esta é a minha pokedex</h1>
+       </div>
+     )
+   }
+ }
 export default App;
