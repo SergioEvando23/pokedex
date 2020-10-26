a5ae1435de12923286e5ed355a86778604f977fc
SergioEvando23 <sergioevandocosta@gmail.com>
2020-10-26 15:13:47 -0300

    clear project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
- import React, { Component } from 'react'
- import Api from './Api'
+import React from 'react'
+
+ const Pokedex = () => {
 
 
- class App extends Component{
-  state = {
-    pokemons: [],
-  }
-  async componentDidMount(){
-    const response = await Api.get('');
-    console.log(response.data);
-
-    this.setState({ pokemons: response.data});
-  }
-
-   render(){
-     return(
-       <div>
-         <h1>Esta é a minha pokedex</h1>
-       </div>
-     )
-   }
  }
-export default App;
+ export default Pokedex()
